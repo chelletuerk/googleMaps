4a1bafb3ced42798ae7791fdca34323fdb588807
chelle tuerk <chelle.tuerk@surveygizmo.com>
2017-08-08 10:41:35 -0600

    remove duplicate files, render google map denver location

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-var script = document.createElement('script');
-   script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
-   document.getElementsByTagName('head')[0].appendChild(script);
+// var script = document.createElement('script');
+//    script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+//    document.getElementsByTagName('head')[0].appendChild(script);
+function initMap() {
+      //  var uluru = {lat: -25.363, lng: 131.044};
+      var denver = {lat: 39.849, lng: -104.673}
+       var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 4,
+         center: denver
+       })
+       var marker = new google.maps.Marker({
+         position: denver,
+         map: map
+       })
+     }
