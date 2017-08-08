51f40920ada3d4d5493956122d5f5faf2f55ef02
chelle tuerk <chelle.tuerk@surveygizmo.com>
2017-08-08 13:52:50 -0600

    push obj values lat and lng into marker array

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-// var script = document.createElement('script');
-//    script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
-//    document.getElementsByTagName('head')[0].appendChild(script);
-// function initMap() {
-//       //  var uluru = {lat: -25.363, lng: 131.044};
-//       var denver = {lat: 39.849, lng: -104.673}
-//        var map = new google.maps.Map(document.getElementById('map'), {
-//          zoom: 4,
-//          center: denver
-//        })
-//        var marker = new google.maps.Marker({
-//          position: denver,
-//          map: map
-//        })
-//      }
+var markerArr = []
 
 addMarkerOnClick = () => {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 2,
     center: {lat: 39.849, lng: -104.673}
   })
-  map.addListener('click', function(e) {
-   placeMarkerAndPanTo(e.latLng, map);
-    });
 
-  placeMarkerAndPanTo = (latLng, map) => {
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
-    });
-    map.panTo(latLng)
-    console.log(marker.position)
-  }
+  map.addListener('click', (e) => {
+   placeMarkerAndPanTo(e.latLng, map);
+    })
+}
+
+placeMarkerAndPanTo = (latLng, map) => {
+  const marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  })
+  map.panTo(latLng)
+  console.log(marker.position.lat())
+  console.log(marker.position.lng())
+  const lat = marker.position.lat()
+  const lng = marker.position.lng()
+  addToMarkerArray(lat, lng)
+}
+
+addToMarkerArray = (lat, lng) => {
+  markerArr.push({lat: lat, lng: lng})
+  console.log(markerArr)
 }
